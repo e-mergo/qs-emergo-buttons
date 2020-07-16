8403e6c27f7a422ff0db0ca874eb469f5afee5e0
Laurens Offereins <lmoffereins@gmail.com>
2020-07-16 23:55:38 +0200

    Add option to set a button's description in its `title` attribute

--- properties.js
+++ properties.js
@@ -60,6 +60,12 @@ define([
 				expression: "optional",
 				ref: "label"
 			},
+			description: {
+				translation: "Common.Description",
+				type: "string",
+				expression: "optional",
+				ref: "description"
+			},
 			calcCond: {
 				label: "Show button if",
 				type: "string",
